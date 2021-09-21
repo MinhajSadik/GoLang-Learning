81fd37e2d1d2b677e756bb9b6a482847a4a3ce31
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-21 21:12:26 +0600

    [add] go some practice code

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import "fmt"
 
 
 func main() {
-    fmt.Println("hello world")
-}
+    fmt.Printf("hello world")
+    var s string = "Hello World"
+    fmt.Println(s)
 
+    // var s string = "Hello, World"
+    for index, character := range(s){
+        fmt.Printf("The character %c is in position %d \n", character, index)
+    }
+}
