e3e9a0f8d596e7d08a2b77cc63a56da30723ea39
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-30 15:21:45 +0600

    [add] multiple constant declear

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	"strings"
 )
 
+// Muiltiple Constant Diclear
+// const (
+//     A int = 1
+//     B = 3.14
+//     C = "Hi!"
+//   )
+
 // func myEarning(name string) string{
 //     return name
 // }
